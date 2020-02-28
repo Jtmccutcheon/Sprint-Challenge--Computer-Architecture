8b0ef65a60467ed0eee56829acf4eb4d4b081972
unknown <jt.mccutcheon@live.com>
2020-02-28 12:58:46 -0500

    mvp

---
+++ ls8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Main."""
+
+import sys
+from cpu import *
+print(sys.argv)
+
+cpu = CPU()
+
+if len(sys.argv) != 2:
+    print('Invalid input.')
+    sys.exit(1)
+if len(sys.argv) == 2:
+    print(f'Running program: {sys.argv[0]}')
+    print(f'Running program: {sys.argv[1]}')
+    # sys.argv[1]
+    cpu = CPU()
+    cpu.load(sys.argv[1])
+    cpu.run()
